054054b607f59441d1c7acbe7f94b4c46c6034d1
Daniel Clerkson <132950305+Daniel-Clerkson@users.noreply.github.com>
2025-10-04 04:00:29 +0100

    Update EstateHero.jsx

--- src/components/EstateComponents/EstateHero.jsx
+++ src/components/EstateComponents/EstateHero.jsx
@@ -7,8 +7,8 @@ const EstateHero = () => {
   return (
     <>
       <EstateHeroNav />
-      <EstateImages estate={window.location.href.substr(47)} />
-      <EstateDetails estate={window.location.href.substr(47)} />
+      <EstateImages estate={window.location.href.substr(26)} />
+      <EstateDetails estate={window.location.href.substr(26)} />
     </>
   );
 };
